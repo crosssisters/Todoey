e693bdcce877f66428dfe4b00115d7d06f258c33
deca <deca@ognens-air.lan>
2019-01-15 17:24:22 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by deca on 1/15/19.
+//  Copyright © 2019 Elena&Angela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
